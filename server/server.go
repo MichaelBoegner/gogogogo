a4763021f61e8bb63018a9bf9ae3fb2c27f33e81
michaelboegner <cmikeb@gmail.com>
2023-08-19 14:28:52 -0400

    func received handler struct to open test file on os to demonstrate process overload if not careful with garbage collection. Undeertanding of process ids, counting files open, and killing processes.

--- server/server.go
+++ server/server.go
@@ -4,18 +4,20 @@ import (
     "log"
     "net/http"
     "time"
+	"os"
 )
 
 func main() {
     // create a server
     myServer := &http.Server{
         // set the server address
-        Addr: "127.0.0.1:8080",
+        Addr: "127.0.0.1:8084",
         // define some specific configuration
         ReadTimeout:  10 * time.Second,
         WriteTimeout: 10 * time.Second,
-		Handler: &myHandler{},
+        Handler:      &myHandler{},
     }
+
     // launch the server
     log.Fatal(myServer.ListenAndServe())
 }
@@ -24,9 +26,10 @@ type myHandler struct {
 }
 
 func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    toSend := []byte("<html><head></head><body>Hello</hello></html>")
-    _, err := w.Write(toSend)
+    _, err := os.Open("./temp/test.go")
     if err != nil {
-        log.Printf("error while writing on the body %s", err)
+        log.Println("error file open ", err)
+    } else {
+        log.Println("file opened")
     }
 }
