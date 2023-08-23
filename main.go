39d047c9245965118637db3689d82d5ec3189668
michaelboegner <cmikeb@gmail.com>
2023-08-23 19:17:45 -0400

    basic example of pointers and referencing

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"test/email"
-	"test/invoice"
+	"fmt"
 )
 
 func main() {
+	i, j := 42, 2701
 
-	// first reservation
-	email.Contents()
-	email.Send()
-	invoice.Create()
+	p := &i         // point to i
+	fmt.Println(&i) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 }
