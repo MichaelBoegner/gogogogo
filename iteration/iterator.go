33491895f9d7c89655885e5d24fed468407ef4f9
michaelboegner <cmikeb@gmail.com>
2023-09-03 13:43:27 -0400

    refactored code to remove redundant for loop. Though the use case isn't very real world, still cleaner

--- iteration/iterator.go
+++ iteration/iterator.go
@@ -4,20 +4,13 @@ import "fmt"
 
 func Repeat(letter string, times int) (repeated string) {
 	if times == 0 {
-		for i := 0; i < 5; i++ {
-			if i < 4 {
-				repeated += letter + ","
-			} else {
-				repeated += letter
-			}
-		}
-	} else {
-		for i := 0; i < times; i++ {
-			if i < times-1 {
-				repeated += letter + ","
-			} else {
-				repeated += letter
-			}
+		times = 5
+	}
+	for i := 0; i < times; i++ {
+		if i < times-1 {
+			repeated += letter + ","
+		} else {
+			repeated += letter
 		}
 	}
 	fmt.Println(repeated)
