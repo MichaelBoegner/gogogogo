6ae35d0def763f91bbb0a2b0bce20ea48bfb32e6
michaelboegner <cmikeb@gmail.com>
2023-09-03 15:15:35 -0400

    wrote test for 'Sum will take an array of numbers and return the total'

---
+++ arrays/sum_test.go
@@ -0,0 +1,16 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("Sum will take an array of numbers and return the total", func(t *testing.T) {
+		array := []int{1, 2, 3}
+		got := Sum(array)
+		want := 6
+
+		if got != want {
+			t.Errorf("Got %d back, but wanted %d", got, want)
+		}
+	})
+
+}
