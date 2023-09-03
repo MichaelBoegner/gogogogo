86f3801d34af9081fc197d7a3237675d6dd5664a
michaelboegner <cmikeb@gmail.com>
2023-09-03 13:41:07 -0400

    all tests passing

--- iteration/iterator.go
+++ iteration/iterator.go
@@ -11,6 +11,14 @@ func Repeat(letter string, times int) (repeated string) {
 				repeated += letter
 			}
 		}
+	} else {
+		for i := 0; i < times; i++ {
+			if i < times-1 {
+				repeated += letter + ","
+			} else {
+				repeated += letter
+			}
+		}
 	}
 	fmt.Println(repeated)
 	return repeated
