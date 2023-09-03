c4f984b8cf4da58a39030be0f41cf01224b86542
michaelboegner <cmikeb@gmail.com>
2023-09-03 13:32:20 -0400

    First benchmark added. Also added breaking test for 'caller determines number of repeats'

--- iteration/iterator_test.go
+++ iteration/iterator_test.go
@@ -3,10 +3,26 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	want := "a,a,a,a,a"
+	t.Run("single letter", func(t *testing.T) {
+		got := Repeat("a")
+		want := "a,a,a,a,a"
 
-	if want != got {
-		t.Errorf("got %q but want %q", got, want)
+		if want != got {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
+	t.Run("caller determines number of repeats", func(t *testing.T) {
+		got := Repeat("a", 2)
+		want := "a,a"
+
+		if got != want {
+			t.Errorf("got %q, but wanted %q", got, want)
+		}
+	})
+}
+
+func BenchmarkName(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
 	}
 }
