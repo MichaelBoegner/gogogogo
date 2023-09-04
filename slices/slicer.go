88a0b9f86a36ec212679272f33b35a3d5dafbb3d
michaelboegner <cmikeb@gmail.com>
2023-09-04 09:56:08 -0400

    SummAll() passing test

--- slices/slicer.go
+++ slices/slicer.go
@@ -1,5 +1,7 @@
 package slices
 
+import "fmt"
+
 func Slice(sliced []int) (sum int) {
 	for _, num := range sliced {
 		sum += num
@@ -8,6 +10,16 @@ func Slice(sliced []int) (sum int) {
 }
 
 func SumAll(sliceA []int, sliceB []int) (sliceSum []int) {
+	sliceSumA, sliceSumB := 0, 0
 
-	return nil
+	for _, num := range sliceA {
+		sliceSumA += num
+	}
+	sliceSum = append(sliceSum, sliceSumA)
+	for _, num := range sliceB {
+		sliceSumB += num
+	}
+	sliceSum = append(sliceSum, sliceSumB)
+	fmt.Println(sliceSum)
+	return sliceSum
 }
