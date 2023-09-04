fa4aed606bf6fb63645164b1558db919456f822c
michaelboegner <cmikeb@gmail.com>
2023-09-04 13:43:39 -0400

    shape Shape added to TestArea()

--- structsmethodsinterfaces/geometry_test.go
+++ structsmethodsinterfaces/geometry_test.go
@@ -15,22 +15,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Area(rectangle Rectangle) should take a height and width of a rectangle and return the area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
 
-		if got != want {
-			t.Errorf("got %.2f, but wanted %.2f", got, want)
-		}
-	})
-	t.Run("Area(circle Circle) should take the radius of a circle and return the area", func(t *testing.T) {
-		circle := Circle{5.0}
-		got := circle.Area()
-		want := 78.53981633974999
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g, but wanted %g", got, want)
 		}
+	}
+
+	t.Run("Area(rectangle Rectangle) should take a height and width of a rectangle and return the area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+	})
+	t.Run("Area(circle Circle) should take the radius of a circle and return the area", func(t *testing.T) {
+		circle := Circle{5.0}
+		checkArea(t, circle, 78.53981633974999)
 	})
 }
