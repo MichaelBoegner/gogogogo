f78012055591509220124c3fd3fc76446c16c074
michaelboegner <cmikeb@gmail.com>
2023-09-05 18:45:57 -0400

    learned how to add descriptive types. Added type Bitcoin float64 for descriptive purposes.

--- wallet/bitcoin_wallet.go
+++ wallet/bitcoin_wallet.go
@@ -2,16 +2,18 @@ package wallet
 
 import "fmt"
 
+type Bitcoin float64
+
 type Wallet struct {
-	total float64
+	total Bitcoin
 }
 
-func (w *Wallet) Deposit(deposit float64) {
+func (w *Wallet) Deposit(deposit Bitcoin) {
 	w.total += deposit
 	fmt.Printf("address of total in main file is %v \n", &w.total)
 }
 
-func (w *Wallet) Balance() float64 {
+func (w *Wallet) Balance() Bitcoin {
 	fmt.Printf("address of total in Ballance in main file is %v \n", &w.total)
 	return w.total
 }
--- wallet/bitcoin_wallet_test.go
+++ wallet/bitcoin_wallet_test.go
@@ -8,12 +8,12 @@ import (
 func TestBitcoinWallet(t *testing.T) {
 	t.Run("wallet should let us deposit Bitcoin", func(t *testing.T) {
 		wallet := Wallet{}
-		wallet.Deposit(0.10)
+		wallet.Deposit(Bitcoin(.10))
 
 		got := wallet.Balance()
 		fmt.Printf("address of total in test is %v \n", &got)
 
-		want := 0.10
+		want := Bitcoin(0.10)
 
 		if got != want {
 			t.Errorf("got %g, but wanted %g", got, want)
