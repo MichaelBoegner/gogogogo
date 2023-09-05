07dd5b1e9b902aeac456256e7a7a3e4c06dddbf2
michaelboegner <cmikeb@gmail.com>
2023-09-05 18:42:04 -0400

    lesson on struct pointers and the fact that they are automatically dereferenced. I'll need to do a little more work with these to get them down perfectly, but I understand the principle.

--- wallet/bitcoin_wallet.go
+++ wallet/bitcoin_wallet.go
@@ -1,13 +1,17 @@
 package wallet
 
+import "fmt"
+
 type Wallet struct {
-	Total float64
+	total float64
 }
 
-func (w Wallet) Deposit(deposit float64) {
-	w.Total += deposit
+func (w *Wallet) Deposit(deposit float64) {
+	w.total += deposit
+	fmt.Printf("address of total in main file is %v \n", &w.total)
 }
 
-func (w Wallet) Balance() (balance float64) {
-	return 0
+func (w *Wallet) Balance() float64 {
+	fmt.Printf("address of total in Ballance in main file is %v \n", &w.total)
+	return w.total
 }
--- wallet/bitcoin_wallet_test.go
+++ wallet/bitcoin_wallet_test.go
@@ -1,6 +1,9 @@
 package wallet
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestBitcoinWallet(t *testing.T) {
 	t.Run("wallet should let us deposit Bitcoin", func(t *testing.T) {
@@ -8,6 +11,8 @@ func TestBitcoinWallet(t *testing.T) {
 		wallet.Deposit(0.10)
 
 		got := wallet.Balance()
+		fmt.Printf("address of total in test is %v \n", &got)
+
 		want := 0.10
 
 		if got != want {
