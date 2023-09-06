0bfe3d51da327c8a11f2424f3a916c12ebc68fd6
michaelboegner <cmikeb@gmail.com>
2023-09-06 18:01:22 -0400

    Withdraw() test built

--- wallet/bitcoin_wallet_test.go
+++ wallet/bitcoin_wallet_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestBitcoinWallet(t *testing.T) {
-	t.Run("wallet should let us deposit Bitcoin", func(t *testing.T) {
+	t.Run("wallet should let us deposit Bitcoin and see the balance", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(.10))
 
@@ -14,6 +14,18 @@ func TestBitcoinWallet(t *testing.T) {
 		fmt.Printf("address of total in test is %v \n", &got)
 
 		want := Bitcoin(0.10)
+
+		if got != want {
+			t.Errorf("got %s, but wanted %s", got, want)
+		}
+	})
+	t.Run("wallet should let us Withdraw Bitcoin", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(.10))
+		wallet.Withdraw(Bitcoin(.05))
+
+		got := wallet.Balance()
+		want := Bitcoin(.05)
 
 		if got != want {
 			t.Errorf("got %s, but wanted %s", got, want)
