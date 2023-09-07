20615f75fbf1c1e92e6733f28e52c8681db55b11
michaelboegner <cmikeb@gmail.com>
2023-09-07 19:07:00 -0400

    refactored test to consolidate got want Ballance() assertion into single function

--- wallet/bitcoin_wallet_test.go
+++ wallet/bitcoin_wallet_test.go
@@ -5,27 +5,25 @@ import (
 )
 
 func TestBitcoinWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s, but wanted %s", got, want)
+		}
+	}
+
 	t.Run("wallet should let us deposit Bitcoin and see the balance", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s, but wanted %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("wallet should let us Withdraw Bitcoin", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		wallet.Withdraw(Bitcoin(5))
 
-		got := wallet.Balance()
-		want := Bitcoin(5)
-
-		if got != want {
-			t.Errorf("got %s, but wanted %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(5))
 	})
 }
