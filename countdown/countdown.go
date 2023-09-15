b53e8ad7a9103ae98266f29f36580c57c50a7115
michaelboegner <cmikeb@gmail.com>
2023-09-14 21:22:26 -0400

    added spySleeper and sleeper interface

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -5,6 +5,18 @@ import (
 	"fmt"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
 func Countdown(buffer *bytes.Buffer) {
 	fmt.Fprintf(buffer, "3")
 }
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -7,14 +7,23 @@ import (
 
 func TestCountdown(t *testing.T) {
 	t.Run("print 3", func(t *testing.T) {
-		buffer := bytes.Buffer{}
-		Countdown(&buffer)
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
 
 		got := buffer.String()
-		want := "3"
+		want := `3
+2
+1
+Go!`
 
 		if got != want {
-			t.Errorf("got %q, wanted %q", got, want)
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 		}
 	})
 
