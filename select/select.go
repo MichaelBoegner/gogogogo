499fac6921f15fe464f55ea56eca120908dac6ad
michaelboegner <cmikeb@gmail.com>
2023-09-23 12:55:12 -0400

    refactored code to dry up responseTime() measurement into function

--- select/select.go
+++ select/select.go
@@ -6,17 +6,18 @@ import (
 )
 
 func Racer(URLA, URLB string) string {
-	startA := time.Now()
-	_, _ = http.Get(URLA)
-	respADuration := time.Since(startA)
+	aTime := responseTime(URLA)
+	bTime := responseTime(URLB)
 
-	startB := time.Now()
-	_, _ = http.Get(URLB)
-	respBDuration := time.Since(startB)
-
-	if respADuration < respBDuration {
+	if aTime < bTime {
 		return URLA
 	} else {
 		return URLB
 	}
+}
+
+func responseTime(URL string) time.Duration {
+	start := time.Now()
+	_, _ = http.Get(URL)
+	return time.Since(start)
 }
