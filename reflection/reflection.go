fa0079b5bb622ab6354a07837ed98e4dac4740a0
michaelboegner <cmikeb@gmail.com>
2023-09-30 10:50:02 -0400

    Walk() passes one string field from x to func and passes test

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,11 @@
 package reflection
 
-func Walk(x interface{}, fn func(string)) {
+import (
+	"reflect"
+)
 
+func Walk(x interface{}, fn func(string)) {
+	reflected := reflect.ValueOf(x)
+	field := reflected.Field(0)
+	fn(field.String())
 }
