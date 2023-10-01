2753117c7c8a3fb812a84863790322e5b0383c01
michaelboegner <cmikeb@gmail.com>
2023-10-01 13:39:51 -0400

    wrote test for concurency and abstracted assertAmount

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,17 +1,41 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
-	t.Run("counter that is safe to use concurrently.", func(t *testing.T) {
+	t.Run("counter counts to three.", func(t *testing.T) {
 		counter := Counter{}
 		counter.Increment()
 		counter.Increment()
 		counter.Increment()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, but wanted %d", counter.Value(), 3)
-		}
-
+		assertAmount(t, counter.Value(), 3)
 	})
+	t.Run("counter that's safe to run concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Increment()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertAmount(t, counter.Value(), wantedCount)
+	})
+}
+
+func assertAmount(t *testing.T, got, want int) {
+	t.Helper()
+	if got != 3 {
+		t.Errorf("got %d, but wanted %d", got, want)
+	}
 }
