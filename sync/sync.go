4ed0fc7f71d1b0a8485c8de6dd8cd08e854e8f4c
michaelboegner <cmikeb@gmail.com>
2023-10-01 09:03:38 -0400

    counter struct/methods pass test

--- sync/sync.go
+++ sync/sync.go
@@ -4,10 +4,10 @@ type Counter struct {
 	amount int
 }
 
-func (c Counter) Increment() {
+func (c *Counter) Increment() {
 	c.amount += 1
 }
 
-func (c Counter) Value() int {
+func (c *Counter) Value() int {
 	return c.amount
 }
