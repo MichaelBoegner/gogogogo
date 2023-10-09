9839cf4d9e577bd774efe9c95d21686af5dfd339
michaelboegner <cmikeb@gmail.com>
2023-10-09 12:49:11 -0400

    basic understanding of context.WithCancel() and Select{case <-ctx.Done(): case do work:} cycle

--- context/context_playground.go
+++ context/context_playground.go
@@ -1,5 +1,42 @@
-package context
+package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
-func firstAction(ctx context.Context)
+func dataSender(ctx context.Context) {
+	ctx, cancelCtx := context.WithCancel(ctx)
+	numChan := make(chan int)
+	go dataHandler(ctx, numChan)
+	for num := 1; num <= 5; num++ {
+		numChan <- num
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("dataSender finished")
+}
+
+func dataHandler(ctx context.Context, numChan <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("\n ctx.Done processed: %v \n", ctx)
+			if err := ctx.Err(); err != nil {
+				fmt.Printf("The ctx.Done() processed because: %v\n", err)
+			}
+			return
+		case result := <-numChan:
+			fmt.Printf("Printing result: %d \n", result)
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "parentKey", "parentValue")
+	dataSender(ctx)
+}
